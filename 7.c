479cc124a44f21f7b365e7e6dc5cb8f143f4e589
openmhw <510492956@qq.com>
2017-02-08 16:19:18 +0800

    m 7.c

--- 7.c
+++ 7.c
@@ -1,4 +1,4 @@
-#define build1
+#define build2
 #ifdef build1
 #include <stdio.h>
 #include <malloc.h>
@@ -13,6 +13,18 @@ int main(int argc,char* argv[])
     printf("%d\n",red); 
     printf("%d\n",blue); 
     printf("%d\n",green); 
+    return 0;
+}
+#endif
+#ifdef build2
+#include <stdio.h>
+typedef int INT32;
+int main()
+{
+    int i=0;
+    int* p=&i;
+    INT32* p32=&i;
+    int* pc=p32;
     return 0;
 }
 #endif
