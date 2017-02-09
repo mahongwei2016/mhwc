88ee6c43342aca9ca0e2a468563e8fafb1f0ab39
openmhw <510492956@qq.com>
2017-02-09 11:06:36 +0800

    m 13.c

--- 13.c
+++ 13.c
@@ -1,11 +1,21 @@
-#define build1
+#define build2
 #ifdef build1
 #include <stdio.h>
 int main()
 {
     int i=3;
     int x=(++i)+(++i)+(++i);
-    printf("x=%d\n",x);
+    printf("build1 x=%d\n",x);
+    return 0;
+}
+#endif
+#ifdef build2
+#include <stdio.h>
+int main()
+{
+    int i=3;
+    int x=(++i,x++,i+10);
+    printf("build2  x=%d\n",x);
     return 0;
 }
 #endif
