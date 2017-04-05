81fffca3d74236ae1b0ae928ecbd5d7e56b1cf0a
mahongwei2016 <510492956@qq.com>
2017-04-05 10:50:32 +0800

    modify 22.c

--- 22.c
+++ 22.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+int main(int argc,char** argv[])
+{
+    int a[5]={1,2};
+    int b[]={1,2};
+    printf("%d %d\n",sizeof(a),sizeof(a)/sizeof(*a));
+    printf("%d %d\n",sizeof(b),sizeof(b)/sizeof(*b));
+    int i=0;
+    for(i=0;i<5;i++)
+    {
+    	printf("%d\n",a[i]);    
+    }
+}
